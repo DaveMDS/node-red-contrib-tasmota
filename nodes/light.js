8b1976490071a13a33069d8337e9b46e2c97caf2
Dave Andreoli <dave@gurumeditation.it>
2020-04-13 13:02:00 +0200

    Initial rework of the Light node
    Redone all the logic of input message handling, it now support 3 different modes:
    MODE 1: simple on/off/toggle (without topic and simple-typed payload)
    MODE 2: topic mode (with simple-typed payload)
    MODE 3: object payload (without topic)
    
    Currently accept the following commands (in both mode 2 and 3):
    on: true/false, 1/0, on/off, toggle (not case sensitive) (alias: state)
    bright: 0-100 (aliases: brightness, dimmer)
    ct: 500-153 mired, 2000-6500 kelvin or 0-100 percent (warm to cold) (alias: colorTemp)
    
    Commands for colors will come soon...stay tuned

--- nodes/light.js
+++ nodes/light.js
@@ -26,35 +26,117 @@ module.exports = function (RED) {
     }
 
     onNodeInput (msg) {
-      const payload = msg.payload
+      var on, bright, ct
 
-      // Switch On/Off for booleans and 1/0 (int or str)
-      if (payload === true || payload === 1 || payload === '1') {
-        this.MQTTPublish('cmnd', 'POWER', onValue)
-        return
-      }
-      if (payload === false || payload === 0 || payload === '0') {
-        this.MQTTPublish('cmnd', 'POWER', offValue)
-        return
+      // MODE 1: simple on/off/toggle (without topic)
+      if (!msg.topic &&
+          (typeof msg.payload === 'number' ||
+           typeof msg.payload === 'boolean' ||
+           typeof msg.payload === 'string')) {
+        on = msg.payload
       }
 
-      // String payload: on/off, true/false, toggle (not case sensitive)
-      if (typeof payload === 'string') {
-        switch (payload.toLowerCase()) {
-          case 'on':
-          case 'true':
-            this.MQTTPublish('cmnd', 'POWER', onValue)
-            return
-          case 'off':
-          case 'false':
-            this.MQTTPublish('cmnd', 'POWER', offValue)
-            return
-          case 'toggle':
-            this.MQTTPublish('cmnd', 'POWER', toggleValue)
-            return
+      // MODE 2: topic mode (with simple-typed payload)
+      if (msg.topic &&
+          (typeof msg.payload === 'boolean' ||
+           typeof msg.payload === 'number' ||
+           typeof msg.payload === 'string')) {
+        if (msg.topic === 'on' || msg.topic === 'state') {
+          on = msg.payload
+        }
+        if (msg.topic === 'bright' || msg.topic === 'brightness' || msg.topic === 'dimmer') {
+          bright = msg.payload
+        }
+        if (msg.topic === 'ct' || msg.topic === 'colorTemp') {
+          ct = msg.payload
         }
       }
 
+      // MODE 3: object payload (without topic)
+      if (!msg.topic && typeof msg.payload === 'object') {
+        // on (aliases: state)
+        if (typeof msg.payload.on !== 'undefined') {
+          on = msg.payload.on
+        }
+        if (typeof msg.payload.state !== 'undefined') {
+          on = msg.payload.state
+        }
+        // bright (aliases: brightness, dimmer)
+        if (typeof msg.payload.bright !== 'undefined') {
+          bright = msg.payload.bright
+        }
+        if (typeof msg.payload.brightness !== 'undefined') {
+          bright = msg.payload.brightness
+        }
+        if (typeof msg.payload.dimmer !== 'undefined') {
+          bright = msg.payload.dimmer
+        }
+        // ct (aliases: colorTemp)
+        if (typeof msg.payload.ct !== 'undefined') {
+          ct = msg.payload.ct
+        }
+        if (typeof msg.payload.colorTemp !== 'undefined') {
+          ct = msg.payload.colorTemp
+        }
+      }
+
+      // did we found something usefull?
+      if (on === undefined && bright === undefined && ct === undefined) {
+        this.warn('Invalid message received on input')
+        return
+      }
+
+      // on: true/false, 1/0, on/off, toggle (not case sensitive)
+      if (on !== undefined) {
+        switch (on.toString().toLowerCase()) {
+          case '1':
+          case 'on':
+          case 'true':
+            this.MQTTPublish('cmnd', 'POWER', onValue)
+            break
+          case '0':
+          case 'off':
+          case 'false':
+            this.MQTTPublish('cmnd', 'POWER', offValue)
+            break
+          case 'toggle':
+            this.MQTTPublish('cmnd', 'POWER', toggleValue)
+            break
+          default:
+            this.warn('Invalid value for the \'on\' command (should be: true/false, 1/0, on/off or toggle)')
+        }
+      }
+
+      // bright: 0-100
+      if (bright !== undefined) {
+        bright = parseInt(bright)
+        if (isNaN(bright) || bright < 0 || bright > 100) {
+          this.warn('Invalid value for the \'bright\' command (should be: 0-100)')
+        } else {
+          this.MQTTPublish('cmnd', 'Dimmer', bright.toString())
+        }
+      }
+
+      // ct: 500-153, 2000-6500, 0-100 (warm to cold)
+      if (ct !== undefined) {
+        ct = parseInt(ct)
+        if (isNaN(ct)) {
+          this.warn('Invalid value for the \'ct\' command (should be: 0-100, 2000-6500 or 500-153)')
+        } else if (ct >= 153 && ct <= 500) { // ct in mired (cold to warm)
+          this.MQTTPublish('cmnd', 'CT', ct.toString())
+        } else if (ct >= 0 && ct <= 100) { // ct in percent (warm to cold)
+          ct = 100 - ct
+          ct = Math.floor(((ct / 100) * (500 - 153)) + 153)
+          this.MQTTPublish('cmnd', 'CT', ct.toString())
+        } else if (ct >= 2000 && ct <= 6500) { // ct in kelvin (warm to cold)
+          ct = Math.floor(1000000 / ct)
+          this.MQTTPublish('cmnd', 'CT', ct.toString())
+        } else {
+          this.warn('Invalid value for the \'ct\' command (should be: 0-100, 2000-6500 or 500-153)')
+        }
+      }
+
+      /*
       // combined hsv and power payload object
       if (typeof payload === 'object') {
         if (payload.POWER) {
@@ -74,8 +156,7 @@ module.exports = function (RED) {
         }
         return
       }
-
-      this.warn('Invalid payload received on input')
+      */
     }
 
     onStat (mqttTopic, mqttPayloadBuf) {
